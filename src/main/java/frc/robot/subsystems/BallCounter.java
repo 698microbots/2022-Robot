47ca892d9df9136c275ca19238d84aa6a401273f
Rewand727 <atran255@gmail.com>
2022-03-09 15:44:14 -0700

    Counter Test 1

--- src/main/java/frc/robot/subsystems/BallCounter.java
+++ src/main/java/frc/robot/subsystems/BallCounter.java
@@ -6,31 +6,42 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Timer;
+
 
 
 public class BallCounter extends SubsystemBase {
   /** Creates a new BallCounter. */
-  
   
   private DigitalInput input = new DigitalInput(Constants.PortID1); // class for port 1
   private DigitalInput input2 = new DigitalInput(Constants.PortID2); // class for port 2
   
   private static int totalBalls = 0; // total ball counter, changes with current
 
+  private boolean portID1Counted = true;
+  private boolean portID2Counted = true;
+
   
   public BallCounter() {}
 
+  
   public void ballsIn() {
-    if (input.get() == true) {
-      totalBalls += 1;}
+    
+    if (input.get() == true && portID1Counted == true) {
+      totalBalls += 2;
+      totalBalls -= 1;
+    }
     }
 
   
   
   public void ballsOut() {
-    if (input2.get() == true) {
-      totalBalls -= 1;}
+    if (input2.get() == true && portID2Counted == true) {
+      totalBalls -= 2;
+      totalBalls += 1;
+    }
     }
 
     public static int getBalls() {
